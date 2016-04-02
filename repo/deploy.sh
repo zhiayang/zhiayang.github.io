ac5ce45deae35979985dd55a60e9575ef4384a3b
zhiayang <zhiayang@gmail.com>
2016-04-02 11:39:40 +0800

    repo update

--- repo/deploy.sh
+++ repo/deploy.sh
@@ -3,67 +3,12 @@
 # handle packages file.
 
 echo "" > Packages
-
-cd debs
-
-for PACKAGE in $(ls *.desc)
-do
-	NAME=$(basename $PACKAGE .desc)
-	echo "Package: $NAME"
-
-	printf "" > "$NAME.pack"
-
-	# the package file contains the description for all versions.
-	for DEB in $(ls $NAME*.deb)
-	do
-		printf "    DEB File: $DEB\n"
-
-		VERSION=$(echo $DEB | cut -d _ -f 2)
-
-		# this is quite gross
-		# but we need to unpackage the deb, edit the control file to reflect the correct version,
-		# and then re-deb it.
-
-		# mkdir -p $PACKAGE.folder/DEBIAN
-		# dpkg-deb --extract $DEB $PACKAGE.folder
-		# dpkg-deb --control $DEB $PACKAGE.folder/DEBIAN
-
-		# rm $DEB
-
-		# # edit the control file
-		# # gsed -i '0,/Version:/{//d}' $PACKAGE.folder/DEBIAN/control
-		# # rm $PACKAGE.folder/DEBIAN/control
-		# # mv $PACKAGE.folder/DEBIAN/control.tmp $PACKAGE.folder/DEBIAN/control
-
-		# dpkg-deb --build -Zlzma $PACKAGE.folder $DEB &> /dev/null
-		# rm -r $PACKAGE.folder
+dpkg-scanpackages -m . /dev/null > Packages
 
 
-		# enter the details.
-		printf "Package: %s\n" $NAME >> "$NAME.pack"
-		printf "Version: %s\n" $VERSION >> "$NAME.pack"
-		printf "Filename: ./debs/%s\n" $DEB >> "$NAME.pack"
-		printf "Size: %s\n" "$(stat -f '%z' $DEB)" >> "$NAME.pack"
-		printf "MD5sum: %s\n" "$(md5 -q $DEB)" >> "$NAME.pack"
-		printf "SHA1: %s\n" "$(shasum -a 1 $DEB | awk '{print $1;}')" >> "$NAME.pack"
-		printf "SHA256: %s\n" "$(shasum -a 256 $DEB | awk '{print $1;}')" >> "$NAME.pack"
-		cat $PACKAGE >> "$NAME.pack"
-		printf "\n\n" >> "$NAME.pack"
-
-	done
-
-	printf "\n"
-
-done
-
-cat $(find . -name '*.pack') > ../Packages
-
-
-cd ..
-
-
-printf "Done: Packages.bz2\n"
+printf "Done: Packages\n"
 bzip2 -kf Packages
+printf "Done: Packages.bz2\n"
 
 
 
